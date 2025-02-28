a1caad9229de88adc007fef24d1099a8e66314e3
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-02-28 07:13:41 +0000

    refactor: rename and cleanup modules

--- crates/ram/src/error.rs
+++ crates/ram/src/error.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+use ariadne::{Color, Fmt, Label, Report, ReportKind, Source};
 use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
 
@@ -36,4 +37,60 @@ pub enum Error {
 pub struct UnknownError {
     #[label("here")]
     pub at: SourceSpan,
+}
+
+pub fn handle_parser_errors(src: &str, errors: Vec<chumsky::error::Simple<char>>) {
+    errors.into_iter().for_each(|e| {
+        let msg = if let chumsky::error::SimpleReason::Custom(msg) = e.reason() {
+            msg.clone()
+        } else {
+            format!(
+                "{}{}, expected {}",
+                if e.found().is_some() { "Unexpected token" } else { "Unexpected end of input" },
+                if let Some(label) = e.label() {
+                    format!(" while parsing {}", label)
+                } else {
+                    String::new()
+                },
+                if e.expected().len() == 0 {
+                    "something else".to_string()
+                } else {
+                    e.expected()
+                        .map(|expected| match expected {
+                            Some(expected) => expected.to_string(),
+                            None => "end of input".to_string(),
+                        })
+                        .collect::<Vec<_>>()
+                        .join(", ")
+                },
+            )
+        };
+
+        let report =
+            Report::build(ReportKind::Error, e.span()).with_code(3).with_message(msg).with_label(
+                Label::new(e.span())
+                    .with_message(match e.reason() {
+                        chumsky::error::SimpleReason::Custom(msg) => msg.clone(),
+                        _ => format!(
+                            "Unexpected {}",
+                            e.found()
+                                .map(|c| format!("token {}", c.fg(Color::Red)))
+                                .unwrap_or_else(|| "end of input".to_string())
+                        ),
+                    })
+                    .with_color(Color::Red),
+            );
+
+        let report = match e.reason() {
+            chumsky::error::SimpleReason::Unclosed { span, delimiter } => report.with_label(
+                Label::new(span.clone())
+                    .with_message(format!("Unclosed delimiter {}", delimiter.fg(Color::Yellow)))
+                    .with_color(Color::Yellow),
+            ),
+            chumsky::error::SimpleReason::Unexpected => report,
+            chumsky::error::SimpleReason::Custom(_) => report,
+        };
+
+        report.finish().print(Source::from(&src)).unwrap();
+    });
 }
--- crates/ram/src/lib.rs
+++ crates/ram/src/lib.rs
@@ -1,72 +1,16 @@
 use std::ffi::OsString;
 use std::process::ExitCode;
 
-use ariadne::{Color, Fmt, Label, Report, ReportKind, Source};
 use chumsky::{Parser as ChumskyParser, stream};
 use clap::{CommandFactory, Parser};
 use cli::{Cli, Command};
 pub use error::Error;
+use error::handle_parser_errors;
 use miette::*;
 
 pub mod cli;
 pub mod error;
-pub mod lang;
-
-fn handle_parser_errors(src: &str, errors: Vec<chumsky::error::Simple<char>>) {
-    errors.into_iter().for_each(|e| {
-        let msg = if let chumsky::error::SimpleReason::Custom(msg) = e.reason() {
-            msg.clone()
-        } else {
-            format!(
-                "{}{}, expected {}",
-                if e.found().is_some() { "Unexpected token" } else { "Unexpected end of input" },
-                if let Some(label) = e.label() {
-                    format!(" while parsing {}", label)
-                } else {
-                    String::new()
-                },
-                if e.expected().len() == 0 {
-                    "something else".to_string()
-                } else {
-                    e.expected()
-                        .map(|expected| match expected {
-                            Some(expected) => expected.to_string(),
-                            None => "end of input".to_string(),
-                        })
-                        .collect::<Vec<_>>()
-                        .join(", ")
-                },
-            )
-        };
-
-        let report =
-            Report::build(ReportKind::Error, e.span()).with_code(3).with_message(msg).with_label(
-                Label::new(e.span())
-                    .with_message(match e.reason() {
-                        chumsky::error::SimpleReason::Custom(msg) => msg.clone(),
-                        _ => format!(
-                            "Unexpected {}",
-                            e.found()
-                                .map(|c| format!("token {}", c.fg(Color::Red)))
-                                .unwrap_or_else(|| "end of input".to_string())
-                        ),
-                    })
-                    .with_color(Color::Red),
-            );
-
-        let report = match e.reason() {
-            chumsky::error::SimpleReason::Unclosed { span, delimiter } => report.with_label(
-                Label::new(span.clone())
-                    .with_message(format!("Unclosed delimiter {}", delimiter.fg(Color::Yellow)))
-                    .with_color(Color::Yellow),
-            ),
-            chumsky::error::SimpleReason::Unexpected => report,
-            chumsky::error::SimpleReason::Custom(_) => report,
-        };
-
-        report.finish().print(Source::from(&src)).unwrap();
-    });
-}
+pub mod language;
 
 pub fn main<Args, T>(args: Args) -> Result<ExitCode>
 where
@@ -102,7 +46,7 @@ where
                 .into_diagnostic()
                 .wrap_err(format!("Failed to read file: {}", program))?;
             let (program, errors) =
-                lang::parser().parse_recovery(stream::Stream::from(src.clone()));
+                language::parser().parse_recovery(stream::Stream::from(src.clone()));
             handle_parser_errors(&src, errors);
 
             if ast {
