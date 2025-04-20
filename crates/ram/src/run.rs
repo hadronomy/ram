7c5ae15606b36a78eb12ed5857164a5e491eb841
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-04-20 18:59:45 +0100

    feat: use VecInput and VecOutput when running the program

--- crates/ram/src/run.rs
+++ crates/ram/src/run.rs
@@ -1,11 +1,11 @@
 //! Module for running RAM programs
 
+use std::io::Write;
 use std::path::Path;
 use std::sync::Arc;
 
 use miette::{IntoDiagnostic, Result, miette};
-use ram_vm::io::{StdinInput, StdoutOutput};
-use ram_vm::{VirtualMachine, VmDatabase, VmDatabaseImpl};
+use ram_vm::{VecInput, VecOutput, VirtualMachine, VmDatabase, VmDatabaseImpl};
 
 /// Run a RAM program from a file path
 pub fn run_program(
@@ -24,15 +24,23 @@ pub fn run_program(
         .parse_to_vm_program(&program_text)
         .map_err(|e| miette!("Failed to parse program: {}", e))?;
 
-    // Create input and output
-    let input = StdinInput::new();
-    let output = StdoutOutput::new();
+    // TODO: Improve io API
+    print!("Input: ");
+    std::io::stdout().flush().into_diagnostic()?;
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).into_diagnostic()?;
+
+    let input =
+        VecInput::new(input.split_whitespace().map(|number| number.parse().unwrap()).collect());
+    let output = VecOutput::new();
 
     // Create a virtual machine
     let mut vm = VirtualMachine::new(program, input, output, db);
 
     // Run the program
     vm.run().map_err(|e| miette!("Failed to run program: {}", e))?;
+
+    println!("Output: {:?}", vm.output.values);
 
     Ok(())
 }
